b188b8e1e53f46ad88c2fc0ae339b9718385d0c7
Nishat Shama <nishat.shama@tuple.com.au>
2019-10-07 15:36:20 +1100

    Add photo upload functionalities.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+import { Auth, Storage, API, graphqlOperation } from 'aws-amplify'
+
 import './App.css';
+import PhotoUpload from './Components/PhotoUpload';
+
+Amplify.configure(aws_exports);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PhotoUpload/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
---
+++ src/Components/PhotoUpload.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Auth, Storage, API, graphqlOperation } from 'aws-amplify';
+import {v4 as uuid} from 'uuid';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
+
+import config from '../aws-exports';
+
+const {
+  aws_user_files_s3_bucket_region: region,
+  aws_user_files_s3_bucket: bucket
+} = config;
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+const PutPhoto = `mutation PutPhoto($bucket: String!, $key: String!, $user: String!, $region: String!) {
+  createPhoto(input: {bucket: $bucket, key: $key, username: $user, region: $region}){
+    bucket
+    key
+  }
+}`
+;
+
+export default function PhotoUpload() {
+  const classes = useStyles();
+  const [uploadStatus, setUploadStatus] = useState('');
+
+  const uploadFile = async (file) => {
+    const user = await Auth.currentAuthenticatedUser();
+    const filename = `${uuid()}-${file.name}`;
+    const result = await Storage.put(
+      filename,
+      file,
+      {
+        customPrefix: { public: 'uploads/' },
+        metadata: { owner: user.username }
+      }
+    );
+    const upload = await API.graphql(graphqlOperation(PutPhoto, {bucket: bucket, key: `uploads/${filename}`, user: user.username, region: region}));
+    console.log('result', result);
+    console.log(upload);
+  }
+
+  const onChange = async (e) => {
+    setUploadStatus('Uploading...');
+    let files = [];
+    for (var i = 0; i < e.target.files.length; i++) {
+      files.push(e.target.files.item(i));
+    }
+    await Promise.all(files.map(f => uploadFile(f)));
+    setUploadStatus('Upload Completed!');
+  }
+
+  return (
+    <div >
+      <Input
+        accept="image/*"
+        id="raised-button-file"
+        multiple
+        type="file"
+        style={{ display: 'none' }}
+        onChange={onChange}
+      />
+      <label htmlFor="raised-button-file">
+        <Button raised component="span" variant="contained" className={classes.button}>
+          <AddToPhotosIcon /> &nbsp; Upload Photos
+        </Button>
+      </label>
+      <span>{uploadStatus}</span>
+    </div>
+  )
+}
